a004ba6d03b9a6289c287df37c67dd541138e6d3
Abidur Niloy <niloyabidur@gmail.com>
2022-11-07 16:55:08 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,32 @@ import FilterData from './filter/FilterData'
 import NavMenu from './navMenu/NavMenu'
 import Quiz from './quiz/Quiz'
 import Quiz2 from './quiz/Quiz2'
+import Sidebar from './sidebar/Sidebar'
 import Temp from './temp/temp'
 import Temp2 from './temp/temp2'
 import Weather from './Weather/Weather'
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
 
   return (
-    <div div>
+    <div >
+      <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Sidebar />}>
+      </Route>
+    </Routes>
+  </BrowserRouter>,
     {/* <NavMenu /> */}
         {/* <Quiz /> */}
         {/* <Temp /> */}
         {/* <Advice /> */}
         {/* <Weather /> */}
-<FilterData/>
+{/* <FilterData/> */}
+    {/* <Sidebar /> */}
     </div>
   )
 }
